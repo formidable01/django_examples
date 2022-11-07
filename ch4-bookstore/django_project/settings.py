999e74739fb11a8ea88630849c491eb216c297f6
Neil Jacobson <neil@formidableengineeringconsultants.com>
2022-11-07 13:36:34 -0800

    ch8

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # Third-party
     'crispy_forms',
     'crispy_bootstrap5',
+    'allauth',
+    'allauth.account',
     # Local apps
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
@@ -50,6 +53,19 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # django-crispy-forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'boostrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# django allauth config
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+        'allauth.account.auth_backends.AuthenticationBackend',
+)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
