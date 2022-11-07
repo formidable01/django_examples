2a724a91e74da07b8b1f02100ca11d09acb2fd6a
Neil Jacobson <neil@formidableengineeringconsultants.com>
2022-11-06 20:15:10 -0800

    ch7

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -37,12 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party
+    'crispy_forms',
+    'crispy_bootstrap5',
     # Local apps
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'boostrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
