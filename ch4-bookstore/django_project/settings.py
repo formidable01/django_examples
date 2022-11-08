22ac9870f72bc6f7a5818df7ba8efa4c1b70b6cb
Neil Jacobson <neil@formidableengineeringconsultants.com>
2022-11-07 21:17:28 -0800

    ch-10

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -71,6 +71,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+DEFAULT_FROM_EMAIL = "admin@indieplaya.com"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
