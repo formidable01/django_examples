8f59c57afc880bfa0f276d5d76d3cc2085ac6f79
Neil Jacobson <neil@formidableengineeringconsultants.com>
2022-11-18 00:23:47 -0800

    ch13

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -32,7 +32,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -159,3 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# media settings
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
